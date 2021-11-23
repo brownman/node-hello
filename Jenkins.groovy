09af25815a5ce5516c63e97564a200688fa1b906
brownman <314539+brownman@users.noreply.github.com>
2021-11-23 12:46:35 +0200

    Create Jenkins.groovy

---
+++ Jenkins.groovy
@@ -0,0 +1,28 @@
+#!/bin/groovy
+ 
+
+
+
+node {
+    
+
+
+    stage('check branch') {
+        if (env.BRANCH_NAME == 'master') {
+            echo 'I only execute on the master branch'
+        } else {
+            echo 'I execute elsewhere'
+        }
+    }
+
+        stage('try catch') {
+        try {
+            sh 'exit 1'
+        }
+        catch (exc) {
+            echo 'Something failed, I should sound the klaxons!'
+            throw
+        }
+    }
+
+}
